8ce2d99d7a1c5a2bbcaa525b8af2b36a1813ce78
gambit0070 <146916650+gambit0070@users.noreply.github.com>
2024-10-17 14:59:03 +0800

    Добавил прочтение txt файлов в main

--- simulation.c
+++ simulation.c
@@ -16,4 +16,17 @@ int main(int argc, char *argv[]) {
 		fprintf(stderr, "usage: %s in.txt out.txt\n", argv[0]);
 	}
 	return 0;
+
+	FILE *input_file = fopen(input_filename, "r");
+    if (input_file == NULL) {
+        fprintf(stderr, "Wrong File format.\n", input_filename);
+        return 1;
+}
+
+    FILE *output_file = fopen(output_filename, "w");
+    if (output_file == NULL) {
+        fprintf(stderr, "Wrong File format.\n", output_filename);
+        fclose(input_file); 
+        return 1;
+}
 }
